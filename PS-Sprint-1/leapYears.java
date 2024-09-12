41e5ce1206c8cb891d5cc85d90833b1d9fcc0a7f
udayvaidya13 <udayvaidya13@gmail.com>
2024-09-13 00:54:52 +0530

    problem3 sol

--- PS-Sprint-1/leapYears.java
+++ PS-Sprint-1/leapYears.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
 class leapYears{
-    public static boolean leap_Years(int year){
+    public static String leap_Years(int year){
         if (year % 400 == 0) {
-            return true; // Divisible by 400, it's a leap year
+            return "Leap Year"; // Divisible by 400, it's a leap year
         } else if (year % 100 == 0) {
-            return false; // Divisible by 100 but not by 400, not a leap year
+            return "Not a Leap Year"; // Divisible by 100 but not by 400, not a leap year
         } else if (year % 4 == 0) {
-            return true; // Divisible by 4 but not by 100, it's a leap year
+            return "Leap Year"; // Divisible by 4 but not by 100, it's a leap year
         } else {
-            return false; // Not divisible by 4, it's not a leap year
+            return "Not a Leap Year"; // Not divisible by 4, it's not a leap year
         }
     } 
 
@@ -17,7 +17,7 @@ class leapYears{
         Scanner sc  = new Scanner(System.in);
         int n  = sc.nextInt();
 
-        boolean x = leap_Years(n);
+        String x = leap_Years(n);
 
         System.out.println(x);
 
